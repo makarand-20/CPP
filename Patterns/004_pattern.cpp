275f88939c4b6b777d35c4360d19cbb3d758f012
Makarand Khiste <makarand.code@gmail.com>
2023-09-17 00:19:20 +0530

    13 pattern done

--- Patterns/004_pattern.cpp
+++ Patterns/004_pattern.cpp
@@ -1,10 +1,9 @@
 /*
-10 11 12 13 14 
-15 16 17 18 19 
-20 21 22 23 24 
-25 26 27 28 29 
-30 31 32 33 34 
-35 36 37 38 39 
+1 2 3 4 5 
+6 7 8 9 10 
+11 12 13 14 15 
+16 17 18 19 20 
+21 22 23 24 25 
 */
 
 #include <iostream>
@@ -13,8 +12,8 @@ using namespace std;
 int main()
 {
     int n = 5;
-    int count = 10;
-    for(int i = 0; i <= n; i++){
+    int count = 1;
+    for(int i = 1; i <= n; i++){
         for (int j = 1; j <= n; j++)
         {
             cout<<count<<" ";
