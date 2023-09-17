a2be0bf6c81ed6d26ccc1fa43ad9142dc2c83705
Makarand Khiste <makarand.code@gmail.com>
2023-09-17 20:11:33 +0530

    bitwise operators

---
+++ Bitwise/001_Operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 7, b = 5;
+    cout << "(a & b) = " << (a & b) << endl; 
+    // 7 = 0111, 5 = 0101, 0111 & 0101 = 0101 = 5 👆
+
+    cout << "(a | b) = " << (a | b) << endl;
+    // 7 = 0111, 5 = 0101, 0111 | 0101 = 0111 = 7 👆
+
+    cout << "(a ^ b) = " << (a ^ b) << endl;
+    // XOR =  7 = 0111, 5 = 0101, 0111 ^ 0101 = 0010 = 2 👆
+
+    cout << "(~a) = " << (~a) << endl; 
+    //always return negative of (a+1) 👆
+
+}
