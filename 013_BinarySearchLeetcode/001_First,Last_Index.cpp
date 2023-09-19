99c33ff009e86123c69958c5c0c6a73c67da8da8
Makarand Khiste <makarand.code@gmail.com>
2023-09-19 23:23:19 +0530

    commit

--- 013_BinarySearchLeetcode/001_First,Last_Index.cpp
+++ 013_BinarySearchLeetcode/001_First,Last_Index.cpp
@@ -3,6 +3,50 @@
 #include <iostream>
 using namespace std;
 
+int firstOcc(int arr[], int n, int key){
+    int start = 0;
+    int end = n - 1;
+    int ans = -1;
+
+    while(start <= end){
+        int mid = start + (end - start)/2;
+
+        if(arr[mid] == key){
+            ans = mid;
+            end = mid - 1;
+        }
+        else if(arr[mid] > key){
+            end = mid - 1;
+        }
+        else{
+            start = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int lastOcc(int arr[], int n, int key){
+    int start = 0;
+    int end = n - 1;
+    int ans = -1;
+
+    while(start <= end){
+        int mid = start + (end - start)/2;
+
+        if(arr[mid] == key){
+            ans = mid;
+            start = mid + 1;
+        }
+        else if(arr[mid] < key){
+            start = mid + 1;
+        }
+        else if(arr[mid] > key){
+            end = mid - 1;
+        }
+    }
+    return ans;
+}
+
 int main(){
     int n;
     cout<<"Enter Size of an array : ";
@@ -18,21 +62,13 @@ int main(){
     cout<<"Enter element to find : ";
     cin>>key;
 
-    int start = 0;
-    int end = n - 1;
+    // pair<int,int>ans;
+    // ans.first = firstOcc(arr, n, key);
+    // ans.second = lastOcc(arr, n, key);
 
-    while(start <= end){
-        int mid = start + (end - start)/2;
+    // cout<<"From first index : "<<ans.first<<" & From last index : "<<ans.second<<endl;
 
-        if(arr[mid] == key){
-            cout<<"Element found at position "<< mid + 1 << "  from first & position "<< n - mid << " from right"<<endl;
-            return 0;
-        }
-        else if(arr[mid] > key){
-            end = mid - 1;
-        }
-        else{
-            start = mid + 1;
-        }
-    }
+    cout<<firstOcc(arr, n, key)<<endl;
+    cout<<lastOcc(arr, n, key)<<endl;
+
 }
