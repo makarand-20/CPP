60d286b2b607fcef020440323fa178d120eed647
Makarand Khiste <makarand.code@gmail.com>
2023-09-19 23:54:57 +0530

    binary vdo 13 done

---
+++ 013_BinarySearchLeetcode/003_PickElement.cpp
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+// Find peak element of given array
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter size of array : ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter array elements : ";
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+
+    int start = 0;
+    int end = n - 1;
+
+    while(start < end){
+        int mid = start + (end - start)/2;
+        if(arr[mid] < arr[mid + 1]){
+            start = mid + 1;
+        }
+        else{
+            end = mid;
+        }
+    }
+
+    cout<<"Pick Element : "<<start<<endl;
+}
