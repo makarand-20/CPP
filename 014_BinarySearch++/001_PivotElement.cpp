a41b4c40b32fc38a0d375343d76bc459c0648281
Makarand Khiste <makarand.code@gmail.com>
2023-09-20 00:12:17 +0530

    commit

---
+++ 014_BinarySearch++/001_PivotElement.cpp
@@ -0,0 +1,31 @@
+// Find pivot element of given array
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter size of array : ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter array elements : ";
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+
+    int start = 0;
+    int end = n - 1;
+
+    while(start < end){
+        int mid = start + (end - start)/2;
+        if(arr[mid] >= arr[0]){
+            start = mid + 1;
+        }
+        else{
+            end = mid;
+        }
+    }
+
+    cout<<"Pivot Element : "<<start<<endl;
+}
