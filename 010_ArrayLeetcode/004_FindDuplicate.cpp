2ea22e4760b46e1ffb83a1e7a587e4f9b42d7866
Makarand Khiste <makarand.code@gmail.com>
2023-09-19 15:21:59 +0530

    commit

---
+++ 010_ArrayLeetcode/004_FindDuplicate.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin >> n;
+
+    int ans = 0;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        ans = ans ^ arr[i];
+    }
+
+    for(int i = 1; i < n; i++){
+        ans = ans ^ i;
+    }
+
+    cout << ans << endl;
+}
