bc8ec32148d85693a20a4839e711fda2a79c946e
Makarand Khiste <makarand.code@gmail.com>
2023-09-19 22:39:53 +0530

    commit

--- 012_BinarySearch/001_BinarySearch.cpp
+++ 012_BinarySearch/001_BinarySearch.cpp
@@ -7,34 +7,34 @@ using namespace std;
 
 int main(){
     int n;
-    cout << "Enter the size of the array: ";
-    cin >> n;
+    cout<<"Enter size of an array : ";
+    cin>>n;
 
     int arr[n];
-    cout << "Enter the elements of the array: ";
-    for(int i=0; i<n; i++){
-        cin >> arr[i];
+    cout<<"Enter array elements : ";
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
     }
 
-    int key;
-    cout << "Enter the key to be searched: ";
-    cin >> key;
+    int k;
+    cout<<"Enter element to find : ";
+    cin>>k;
 
     int start = 0;
-    int end = n-1;
+    int end = n - 1;
 
     while(start <= end){
         int mid = start + (end - start)/2;
 
-        if(arr[mid] == key){
-            cout << "Key found at index: " << mid << endl;
+        if(arr[mid] == k){
+            cout<<"Element finds at index "<<mid <<endl;
             return 0;
         }
-        else if(arr[mid] > key){
-            end = mid -1;
+        else if(arr[mid] < k){
+            start = mid + 1;
         }
         else{
-            start = mid + 1;
+            end = mid - 1;
         }
     }
 }
